778a8b61b709c83e60dcd8a82b499bce2a36fa08
Jameel Hassan <jhassan.tck14@gmail.com>
2022-07-17 15:46:30 +0530

    filtering and pattern search

--- main.py
+++ main.py
@@ -12,15 +12,15 @@
 # Compare guess word to actual word and output a pattern -- DONE
 
 
-# To obtain pattern for the Guess Word for further proecssing
+# To obtain pattern for the Guess Word for further processing
 def getPattern(ans_word, guess):
     '''
     :param word: The guessed word
     :param ans_word: The correct answer
     :return: Pattern to assist next guess
         0 - Letter does not exist
-        1 - Letter exits in the same location
-        2 - Letter exists somewhere else
+        2 - Letter exits in the same location
+        1 - Letter exists somewhere else
     Given a GUESS WORD and ANSWER find the pattern it would generate
     '''
 
@@ -35,13 +35,16 @@ def getPattern(ans_word, guess):
 
     for idx, g_letter in enumerate(guess):
         if g_letter == ans_word[idx]:
-            pattern.append('1')
+            # Letter in the same location
+            pattern.append('2')
             letter_set[g_letter] -= 1
         elif g_letter in ans_word:
             if letter_set[g_letter] > 0:
-                pattern.append('2')
+                # Letter exists in different location
+                pattern.append('1')
                 letter_set[g_letter] -= 1
             else:
+                # Letter does not exist
                 pattern.append('0')
         else:
             pattern.append('0')
@@ -52,21 +55,20 @@ def getPattern(ans_word, guess):
 
 
 # To shorten word list based on pattern output
-def searchPattern(ans_word, pattern, word_list):
+def filterWords(ans_word, pattern, word_list):
     '''
     :param ans_word
     :param pattern: The specific pattern
     :param word_list: Set of all words in the list
-    :return: Dict of all word that have the same pattern
+    :return: Set of all word that have the same pattern
     Given a GUESS WORD and a PATTERN find all possible words that could generate that pattern
     '''
 
     matchedWords = set()
-    pattern1 = pattern
-    pattern2 = ''.join(['1' if x == '2' else x for x in pattern1])
 
     for item in word_list:
-        if getPattern(ans_word, item) == pattern1 or getPattern(ans_word, item) == pattern2:
+        # Set each word as the answer, and check the pattern if the ans_word is used
+        if getPattern(item, ans_word) == pattern:
             matchedWords.add(item)
 
     matchedWords.add(ans_word)
@@ -87,12 +89,14 @@ def patternDist(ans_word, word_list):
     for i in range(3):
         for j in range(3):
             for k in range(3):
-                matched_words = set()
-                pattern = str(i) + str(j) + str(k)
-                for word in word_list:
-                    if getPattern(ans_word, word) == pattern:
-                        matched_words.add(word)
-                prob_dist[pattern] = matched_words
+                for l in range(3):
+                    matched_words = 0
+                    pattern = str(i) + str(j) + str(k) + str(l)
+                    for word in word_list:
+                        if getPattern(ans_word, word) == pattern:
+                            matched_words += 1
+
+                    prob_dist[pattern] = matched_words
 
     return prob_dist
 
@@ -103,6 +107,27 @@ def patternDist(ans_word, word_list):
 
 
 
-word_list = {'not', 'vet', 'bat', 'why', 'job', 'cat', 'lot', 'mad', 'cry', 'sit', 'one', 'hat', 'got', 'red',
-             'cut', 'are', 'fog', 'two', 'zoo', 'fit', 'wry', 'ace', 'put'}
+word_list = {'clap', 'clip', 'luck', 'call', 'cute', 'lick', 'step', 'stop', 'site', 'grow', 'show',
+             'meow', 'plot', 'worm', 'stem', 'ease', 'stew', 'home', 'gone', 'moan', 'film', 'fine',
+             'bone', 'brim'}
 
+if __name__ == "__main__":
+    answer = 'luck'
+    guess = 'call'
+
+    pattern = getPattern(answer, guess)
+    pattern_match = filterWords(answer, pattern, word_list)
+    distr = patternDist(answer, word_list)
+
+    print(pattern)
+    print("words that matched the pattern are")
+    for w in pattern_match:
+        print(w)
+
+    print(distr)
+
+    total = 0
+    for count in distr.values():
+        total += count
+
+    print(f"There are {len(word_list)} number of words and {total} in distr")
