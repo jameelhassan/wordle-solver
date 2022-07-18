7001af24994e047d4dd687d81a8f6716b913ad52
Jameel Hassan <jhassan.tck14@gmail.com>
2022-07-18 13:14:55 +0530

    naive solver

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-# TO DO
-# Need the 5 letter word list
+import numpy as np
+import random
 
-# Create Prob Dist of a GUESS WORD
-## Calculate count of words generating each pattern from word_list for the ANSWER_WORD -- DONE
+def get_word_list(filename):
+    '''
+    :param filename: path to txt file with words
+    :return: list of all words
+    '''
+    with open(filename, 'r') as f:
+        word_list = f.read().split('\n')
+
+    return word_list
 
 
-# Given a word and a pattern choose all words that fit that pattern -- DONE: To shorten the word_list
-
-# Given a word calculate entropy
-
-# Compare guess word to actual word and output a pattern -- DONE
-
-
-# To obtain pattern for the Guess Word for further processing
-def getPattern(ans_word, guess):
+def get_pattern(ans_word, guess):
     '''
     :param word: The guessed word
     :param ans_word: The correct answer
@@ -54,80 +53,114 @@ def getPattern(ans_word, guess):
     return pattern
 
 
-# To shorten word list based on pattern output
-def filterWords(ans_word, pattern, word_list):
+def filter_word_list(ans_word, pattern, word_list):
     '''
     :param ans_word
     :param pattern: The specific pattern
     :param word_list: Set of all words in the list
     :return: Set of all word that have the same pattern
-    Given a GUESS WORD and a PATTERN find all possible words that could generate that pattern
+    Filter the word list based on the pattern
+    Given a WORD and a PATTERN find all possible words that could generate that pattern
     '''
 
     matchedWords = set()
 
     for item in word_list:
-        # Set each word as the answer, and check the pattern if the ans_word is used
-        if getPattern(item, ans_word) == pattern:
+        if get_pattern(item, ans_word) == pattern:
             matchedWords.add(item)
 
     matchedWords.add(ans_word)
     return matchedWords
 
 
-# Get the number of words generating each pattern for the given Answer
-def patternDist(ans_word, word_list):
+def get_pattern_dist(test_word, word_list):
     '''
     :param word: Guessed word
     :param word_list: Dict of all words in the list
     :return: Dict of probability values for each pattern
-    Given the ANSWER, calculate the count of each pattern
+    Given a word, what is the probability distribution of all possible patterns
     '''
 
     prob_dist = {}
+    total_words = len(word_list)
 
     for i in range(3):
         for j in range(3):
             for k in range(3):
                 for l in range(3):
-                    matched_words = 0
-                    pattern = str(i) + str(j) + str(k) + str(l)
-                    for word in word_list:
-                        if getPattern(ans_word, word) == pattern:
-                            matched_words += 1
+                    for m in range(3):
+                        matched_words = 0
+                        pattern = str(i) + str(j) + str(k) + str(l) + str(m)
+                        for word in word_list:
+                            if get_pattern(test_word, word) == pattern:
+                                matched_words += 1
 
-                    prob_dist[pattern] = matched_words
+                        prob_dist[pattern] = matched_words/total_words
 
+    # score_sum = sum(prob_dist.values())
+    # for pattern, score in prob_dist.items():
+    #     prob_dist[pattern] = score/score_sum
     return prob_dist
 
 
+def compute_entropy(prob_distr):
+    entropy = 0
+    for pattern, prob in prob_distr.items():
+        if prob != 0:
+            entropy += -prob * np.log(prob)
 
-# def wordProbDist(guess_word, word_list):
+    return entropy
 
-
-
-
-word_list = {'clap', 'clip', 'luck', 'call', 'cute', 'lick', 'step', 'stop', 'site', 'grow', 'show',
-             'meow', 'plot', 'worm', 'stem', 'ease', 'stew', 'home', 'gone', 'moan', 'film', 'fine',
-             'bone', 'brim'}
+#
+# word_list = {'clap', 'clip', 'luck', 'call', 'cute', 'lick', 'step', 'stop', 'site', 'grow', 'show',
+#              'meow', 'plot', 'worm', 'stem', 'ease', 'stew', 'home', 'gone', 'moan', 'film', 'fine',
+#              'bone', 'brim'}
 
 if __name__ == "__main__":
-    answer = 'luck'
-    guess = 'call'
+    ALL_WORDS = 'data/allowed_words.txt'
+    ANS_WORDS = 'data/possible_words.txt'
+    CORRECT = '22222'
 
-    pattern = getPattern(answer, guess)
-    pattern_match = filterWords(answer, pattern, word_list)
-    distr = patternDist(answer, word_list)
+    word_list = get_word_list(ANS_WORDS)
+    answer_words = get_word_list(ANS_WORDS)
+    answer_word = random.choice(answer_words)
+    print(f"There are {len(word_list)} possible words")
+    attempt = 1
 
-    print(pattern)
-    print("words that matched the pattern are")
-    for w in pattern_match:
-        print(w)
+    max_entropy = 0
+    words_traversed = 0
+    for word in word_list:
+        word_prob_distr = get_pattern_dist(word, word_list)
+        word_entropy = compute_entropy(word_prob_distr)
+        if word_entropy > max_entropy:
+            max_entropy = word_entropy
+            guess_word = word
+        words_traversed += 1
+        if words_traversed % 20 == 0:
+            print('Words traversed {}'.format(words_traversed))
 
-    print(distr)
+    pattern = get_pattern(answer_word, guess_word)
+    print(f'Chosen guess word is {word} and the pattern is {pattern}')
 
-    total = 0
-    for count in distr.values():
-        total += count
+    while pattern != CORRECT:
+        word_list = filter_word_list(answer_word, pattern, word_list)
+        print(f"Filtered list has {len(word_list)} words")
 
-    print(f"There are {len(word_list)} number of words and {total} in distr")
+        max_entropy = 0
+        for word in word_list:
+            word_prob_distr = get_pattern_dist(word, word_list)
+            word_entropy = compute_entropy(word_prob_distr)
+            if word_entropy > max_entropy:
+                max_entropy = word_entropy
+                guess_word = word
+        pattern = get_pattern(answer_word, guess_word)
+        attempt += 1
+        print(f'Chosen guess word is {word} and the pattern is {pattern}')
+
+    print(f'''The answer was guessed in {attempt} attempts
+The guessed answer is {guess_word}
+The correct answer is {answer_word}
+''')
+
+
+
